bfe065493ba7faea8f476fbed82fc1d9e405187a
Anas <m.anasseth00@gmail.com>
2024-04-25 17:47:21 +0500

    Initial Setup Completed

---
+++ gateway/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/customer', proxy('http://localhost:8001'))
+app.use('/products', proxy('http://localhost:8002'))
+app.use('/shopping', proxy('http://localhost:8003'))
+app.use('/', proxy('http://localhost:8000'))
+
+app.listen(8000, () => {
+    console.log("Gateway is listening on port 8001 !")
+})
